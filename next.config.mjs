4c3431c67a4b9ebd4208ee665cba1ddef96bf8ee
novorom <37797096+novorom@users.noreply.github.com>
2026-03-08 12:13:38 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "pvi.cersanit.ru",
       },
     ],
-    formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200],
-    imageSizes: [64, 128, 256, 384, 400],
-    minimumCacheTTL: 2592000, // 30 дней кэш
   },
 }
 
