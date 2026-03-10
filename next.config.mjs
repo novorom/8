c5d6615e5d80c2e4aa36b15d64d73c3ec520043c
R <r@MacBook-Pro-R.local>
2026-03-10 17:12:55 +0100

    fix: 301 redirect www to non-www

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,21 @@ const nextConfig = {
         protocol: "https",
         hostname: "pvi.cersanit.ru",
       },
+      {
+        protocol: "https",
+        hostname: "images.weserv.nl",
+      },
     ],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.cersanit-spb.ru" }],
+        destination: "https://cersanit-spb.ru/:path*",
+        permanent: true,
+      },
+    ]
   },
 }
 
