2d33d0eca2fa2fe7123159fa2dfdeaab36e5314d
novorom <37797096+novorom@users.noreply.github.com>
2026-03-18 13:35:37 +0100

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -4,27 +4,30 @@ import Link from "next/link"
 
 export function HeroSection() {
   return (
-    <section className="relative w-full">
+    <section className="relative w-full max-w-[1440px] mx-auto">
 
-      {/* КАРТИНКА БЕЗ ОБРЕЗКИ */}
-      <img
-        src="/hero.jpg"
-        alt="hero"
-        className="w-full h-auto block"
-      />
+      {/* ОБЁРТКА С ПРОПОРЦИЕЙ КАК У КАРТИНКИ */}
+      <div className="relative w-full aspect-[1440/620]">
 
-      {/* КНОПКА "СМОТРЕТЬ КАТАЛОГ" */}
-      <Link
-        href="/catalog"
-        className="absolute left-[6.8%] top-[63.5%] w-[26%] h-[11%]"
-      />
+        <img
+          src="/hero.jpg"
+          alt="hero"
+          className="absolute inset-0 w-full h-full object-cover"
+        />
 
-      {/* КНОПКА "ВСЕ КОЛЛЕКЦИИ" */}
-      <Link
-        href="/collections"
-        className="absolute left-[34%] top-[63.5%] w-[22%] h-[11%]"
-      />
+        {/* КНОПКА 1 */}
+        <Link
+          href="/catalog"
+          className="absolute left-[110px] top-[385px] w-[320px] h-[70px]"
+        />
 
+        {/* КНОПКА 2 */}
+        <Link
+          href="/collections"
+          className="absolute left-[460px] top-[385px] w-[300px] h-[70px]"
+        />
+
+      </div>
     </section>
   )
 }
