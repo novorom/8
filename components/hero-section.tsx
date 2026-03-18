d5ca96130f2dc17aed14c635ff3e394ce0ff661f
novorom <37797096+novorom@users.noreply.github.com>
2026-03-18 13:30:38 +0100

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -6,7 +6,7 @@ export function HeroSection() {
   return (
     <section className="relative w-full">
 
-      {/* КАРТИНКА 1:1 БЕЗ ОБРЕЗКИ */}
+      {/* КАРТИНКА БЕЗ ОБРЕЗКИ */}
       <img
         src="/hero.jpg"
         alt="hero"
@@ -16,13 +16,13 @@ export function HeroSection() {
       {/* КНОПКА "СМОТРЕТЬ КАТАЛОГ" */}
       <Link
         href="/catalog"
-        className="absolute left-[7.5%] top-[60%] w-[22%] h-[10%]"
+        className="absolute left-[6.8%] top-[63.5%] w-[26%] h-[11%]"
       />
 
       {/* КНОПКА "ВСЕ КОЛЛЕКЦИИ" */}
       <Link
         href="/collections"
-        className="absolute left-[31%] top-[60%] w-[20%] h-[10%]"
+        className="absolute left-[34%] top-[63.5%] w-[22%] h-[11%]"
       />
 
     </section>
